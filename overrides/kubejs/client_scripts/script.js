e2e9b27808ec81a622fcbbe8003067206a0d84c5
moyuguguji <108826874+moyuguguji@users.noreply.github.com>
2024-08-20 01:04:21 +0800

    Update script.js

--- overrides/kubejs/client_scripts/script.js
+++ overrides/kubejs/client_scripts/script.js
@@ -15,4 +15,10 @@ onEvent('rei.hide.items', event => {
             text.add(3, "来自赞助者-xuexiye")
         }
     })
+    tooltip.addAdvanced('kubejs:huan_lin_ying',(item, advanced, text) => {
+      text.add(4, Text.darkPurple('你感受到了灵魂深处刺骨的寒意').bold(true))
+    })
+    tooltip.addAdvanced('bosses_of_mass_destruction:brimstone_nectar',(item, advanced, text) => {
+      text.add(2, Text.gold('仅适用于:夜巫妖,虚空之花,地狱之手,黑曜石之柱').bold(true))
+    })
   })
