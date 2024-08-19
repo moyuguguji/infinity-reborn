e40d068812b8b1cc67edbaa490b8a6c9aaade6f6
moyuguguji <108826874+moyuguguji@users.noreply.github.com>
2024-08-19 08:39:41 +0800

    Update end.js

--- overrides/kubejs/server_scripts/end.js
+++ overrides/kubejs/server_scripts/end.js
@@ -46,4 +46,54 @@ onEvent('recipes', event => {
         ],
         "description": "§0Thank You for Playing"
     })
+    event.custom({
+        type: "spectrum:fusion_shrine",
+        "group": "horse_armor",
+        "time": 1000,
+        "experience": 5000.0,
+        "fluid": "spectrum:liquid_crystal",
+        "ingredients": [
+            {
+                "item": 'kubejs:putong_stone_sword',
+                "count": 1
+            },
+            {
+                "item": 'kubejs:sixiang_kaitian',
+                "count": 1
+            },
+            {
+                "item": 'kubejs:jian_li',
+                "count": 1
+            },
+            {
+                "item": 'kubejs:qixing_sword',
+                "count": 1
+            },
+            {
+                "item": 'mcdw:sword_the_starless_night',
+                "count": 1
+            },
+            {
+                "item": 'gobber2:gobber2_sword_sniper',
+                "count": 1
+            },
+            {
+                "item": 'kubejs:upgrade_liuren',
+                "count": 2
+            }
+        ],
+        "result": {
+            "item": 'kubejs:infinity_sword'
+        },
+        "required_advancement": "spectrum:build_fusion_shrine",
+        "world_conditions": [],
+        "start_crafting_effect": "weather_thunder",
+        "during_crafting_effects": [
+            "lightning_around_shrine",
+            "visual_explosions_on_shrine",
+            "lightning_around_shrine",
+            "visual_explosions_on_shrine"
+        ],
+        "description": "§0Thank You for Playing"
+    })
 })
