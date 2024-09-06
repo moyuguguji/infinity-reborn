ace91f5e13d6ee110685151e3abf026655c04e9c
moyuguguji <108826874+moyuguguji@users.noreply.github.com>
2024-09-07 03:43:49 +0800

    Update curios.js

--- overrides/kubejs/server_scripts/curios.js
+++ overrides/kubejs/server_scripts/curios.js
@@ -9,4 +9,5 @@ onEvent('recipes', event => {
   .keepIngredient('kubejs:disintegration_tablet')
   event.shapeless('kubejs:spectral_silt', [Item.of('kubejs:disintegration_tablet').ignoreNBT(),'@utilities' ])
   .keepIngredient('kubejs:disintegration_tablet')
+  event.shapeless(Item.of('patchouli:guide_book', '{"patchouli:book":"patchouli:infinity_book"}'),['minecraft:diamond','minecraft:book']).id('kubejs:infinity_book')
 })
