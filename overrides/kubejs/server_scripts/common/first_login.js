74b74b859ac3020c2766590e61236558856ae2d7
guguji <211958924@qq.com>
2025-02-10 09:19:40 +0800

    补充

--- overrides/kubejs/server_scripts/common/first_login.js
+++ overrides/kubejs/server_scripts/common/first_login.js
@@ -3,12 +3,14 @@ onEvent('player.logged_in', event => {
 	if (event.player.stages.has('nether')) return
 	// 没有则添加该阶段
 	event.player.stages.add('nether')
+	event.player.give(Item.of('emerald_tools:copper_sword', '{Unbreakable:1b}'))
+	event.player.give(Item.of('treeaxe:copper_treeaxe', '{Unbreakable:1b}'))
+	event.player.give(Item.of('emerald_tools:copper_pickaxe', '{Unbreakable:1b}'))
 	event.player.give(Item.of('kubejs:difficulty_changer', '{RepairCost:1}'))
 	event.player.give(Item.of('20x kubejs:chinese_dumpling'))
 	event.player.give(Item.of('2x kubejs:firecracker'))
 	event.player.give(Item.of('5x kubejs:red_packet'))
 	event.player.give(Item.of('10x kubejs:shi_lian'))
-	event.player.give('minecraft:stone_sword')
 	event.player.give("kubejs:starter_bag")
 	event.player.give('dankstorage:dank_2')
 	event.server.runCommandSilent(`scoreboard players set choice nether 1`)
